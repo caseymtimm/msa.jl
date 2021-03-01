b4684bdb5b9d47060e3fac8306cb341504ff674f
Casey M Timm <casey.m.timm@gmail.com>
2021-02-28 21:28:18 -0500

    Updated to be a package Added unit testing Made into a package Cleaned up structure

---
+++ src/MSA.jl
@@ -0,0 +1,83 @@
+module MSA
+
+using DataFrames
+using Statistics
+using Distributions
+
+export gage
+
+function gage(data, p, α)
+    parts = unique(data.Part)
+    appraisers = unique(data.Appraiser)
+    trials = unique(data.Trial)
+
+    n = length(parts)
+    k = length(appraisers)
+    r = length(trials)
+
+    DFo = k - 1
+    DFp = n - 1
+    DFop = DFo * DFp
+    DFe = n * k * (r - 1)
+
+    grand_mean = mean(data.Value)
+
+    parts_means = map(x -> mean(x.Value), groupby(data, :Part))
+    operator_means = map(x -> mean(x.Value), groupby(data, :Appraiser))
+    trial_means = map(x -> mean(x.Value), groupby(data, :Trial))
+
+    SSo = n * r * sum(x -> (x - grand_mean)^2, operator_means)
+    SSp = k * r * sum(x -> (x - grand_mean)^2, parts_means)
+    SSe = sum(factor -> sum(x -> (x - mean(factor.Value))^2, factor.Value), groupby(data, [:Part, :Appraiser]))
+    TSS = sum(x -> (x - grand_mean)^2, data.Value)
+
+    SSop = TSS - (SSo + SSp + SSe)
+
+    MSo = SSo / DFo
+    MSp = SSp / DFp
+    MSop = SSop / DFop
+    MSe = SSe / DFe
+
+    τ² = MSe
+    γ² = (MSop - MSe) / r
+    ω² = (MSo - MSop) / (n * r)
+    σ² = (MSp - MSop) / (k * r)
+
+    EMSo = τ² - r * γ² + n * r * ω²
+    EMSp = τ² - r * γ² + k * r * σ²
+    EMSop = τ² - r * γ²
+    EMSe = τ²
+
+    f = MSop / MSe
+
+    anova = (
+        DF = (o = DFo, p = DFp, op = DFop, e = DFe),
+        SS = (o = SSo, p = SSp, op = SSop, e = SSe),
+        MS = (o = MSo, p = MSp, op = MSop, e = MSe),
+        EMS = (o = EMSo, p = EMSp, op = EMSop, e = EMSe),
+        f = f
+    )
+
+    f_table = FDist(DFop, DFe)
+
+    critical_value = quantile(f_table, 1 - α)
+
+    significant = f > critical_value
+
+    equipment_variation = p * sqrt(MSe)
+    appraiser_variation = p * sqrt((MSo - MSop) / (n * r))
+    interaction = p * sqrt((MSop - MSe) / r)
+
+    RR = τ² + γ² + ω²
+
+    PV = p * sqrt((MSp - MSop) / (k * r))
+
+    TV = sqrt(RR^2 + PV^2)
+
+    precent_repeatability_variation = 100 * ((p * sqrt(τ²)) / (p * TV))
+
+    anova, τ², γ², ω², σ², RR, significant
+
+end
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,59 @@
+using Test
+using MSA
+using CSV
+using DataFrames
+
+data = CSV.File("./testdata.csv") |> DataFrame
+
+p = 5.15
+α = 0.25
+
+anova, τ², γ², ω², σ², RR, significant, precent_study_variation, precent_contribution = gage(data, p, α)
+
+@testset "Anova Table" begin
+    @testset "Degrees of Freedom (DF)" begin
+        @test round(anova.DF.o, digits=5) == 2
+        @test round(anova.DF.p, digits=5) == 9
+        @test round(anova.DF.op, digits=5) == 18
+        @test round(anova.DF.e, digits=5) == 30
+    end
+    @testset "Sum Of Squares (SS)" begin
+        @test round(anova.SS.o, digits=5) == 0.04800
+        @test round(anova.SS.p, digits=5) == 2.05871
+        @test round(anova.SS.op, digits=5) == 0.10367
+        @test round(anova.SS.e, digits=5) == 0.03875
+    end
+    @testset "Means Squared (MS)" begin
+        @test round(anova.MS.o, digits=5) == 0.02400
+        @test round(anova.MS.p, digits=5) == 0.22875
+        @test round(anova.MS.op, digits=5) == 0.00576 # This is really 0.00575 in the book but rounds to 0.00576
+        @test round(anova.MS.e, digits=5) == 0.00129
+    end
+    @test round(anova.f, digits=2) == 4.46 # This is 4.45 in the book, but is really 4.4588
+end
+
+@testset "Results" begin
+    @testset "Estimate of Variance" begin
+        @test round(τ², digits=5) == 0.00129
+        @test round(γ², digits=5) == 0.00223
+        @test round(ω², digits=5) == 0.00091
+        @test round(σ², digits=7) == 0.0371644 # The book has 0.0371641 but I get 0.0371644 is this a rounding error?
+        @test rount(RR, digits=5) == 0.00443
+    end
+    @testset "% Study Variation" begin
+        @test round(precent_study_variation.repeatability, digits=1) == 17.6
+        @test round(precent_study_variation.operator, digits=1) == 14.8
+        @test round(precent_study_variation.interaction, digits=1) == 23.2
+        @test round(precent_study_variation.rr, digits=1) == 32.7
+        @test rount(precent_study_variation.part, digits=1) == 94.5
+    end
+    @testset "% Contribution" begin
+        @test round(precent_contribution.repeatability, digits=1) == 3.1
+        @test round(precent_contribution.operator, digits=1) == 2.2
+        @test round(precent_contribution.interaction, digits=1) == 5.4
+        @test round(precent_contribution.rr, digits=1) == 10.7
+        @test rount(precent_contribution.part, digits=1) == 89.3
+    end
+end
+
+# weave("./GageWorksheet.jmd", doctype="pandoc2pdf", args=(data = data,))
